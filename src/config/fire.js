c360565355efd1c8a187a3bff2fe8be339b1c23e
RLGARAY <ruben33lg@gmail.com>
2024-02-03 12:51:03 +0100

    Hosting Firestore config
    Deploy of first version

---
+++ src/config/fire.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyC0y04xh0lWou9vJERbT9RRhYLeBmrgcZc',
+  authDomain: 'ruben-portfolio.firebaseapp.com',
+  projectId: 'ruben-portfolio',
+  storageBucket: 'ruben-portfolio.appspot.com',
+  messagingSenderId: '108986761923',
+  appId: '1:108986761923:web:5ab6f1c4b0aab9e7aed876',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
