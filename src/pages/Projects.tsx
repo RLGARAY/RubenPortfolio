705406d5b3a338a141d4e106ca4214cb3c0b021c
RLGARAY <ruben33lg@gmail.com>
2024-02-08 00:51:35 +0100

    Pages structure
    Made the structure for the upcoming data in each page

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,11 +1,55 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
 export default function Projects() {
   return (
-    <Box sx={{ display: 'flex', height: 1 }}>
-      <Typography variant="h2">Projects</Typography>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        textAlign: 'center',
+      }}
+    >
+      <Typography variant="h1" gutterBottom>
+        Projectos Personales
+      </Typography>
+      <Grid container spacing={4} sx={{ flexDirection: 'column', padding: 2 }}>
+        <Grid item xs={12} sm={11} md={9} lg={8} sx={{ marginRight: 'auto' }}>
+          <Paper elevation={3} sx={{ padding: 2 }}>
+            <Typography variant="h6" gutterBottom>
+              Experiencia Izquierda
+            </Typography>
+            <Typography variant="body2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+              dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+              mollit anim id est laborum.
+            </Typography>
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12} sm={11} md={9} lg={8} sx={{ marginLeft: 'auto' }}>
+          <Paper elevation={3} sx={{ padding: 2 }}>
+            <Typography variant="h6" gutterBottom>
+              Experiencia derecha
+            </Typography>
+            <Typography variant="body2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+              dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+              mollit anim id est laborum.
+            </Typography>
+          </Paper>
+        </Grid>
+      </Grid>
     </Box>
   );
 }
